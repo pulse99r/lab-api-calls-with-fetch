c109235646830aee8e8464c19f5f03ed729a6f8b
Raymond <raymondjohnson@pursuit.org>
2023-03-23 23:02:03 -0400

    All tests passing!

--- script.js
+++ script.js
@@ -4,28 +4,30 @@ const url = "https://opentdb.com/api_config.php"
 const form  = document.querySelector("form")
 const BASE_URL = "https://opentdb.com/api.php?amount=10"
 form.addEventListener("submit", (event)=>{
-    event.preventDefault();
+  event.preventDefault();
 
-    fetch(BASE_URL)
-    .then((response) => response.json())
-    .then((json) => {
-        json.results.forEach((result) =>{
-            showTriviaResults(result)
+  fetch(BASE_URL)
+  .then((response) => response.json())
+  .then((json) => {
+      json.results.forEach((result) =>{
+          displayTrivia(result)
+          showDifficulty(result)
         })
-    })
-    .catch(showError(error));
-
+        let triviaArticles = document.querySelectorAll('article.card')
+        showCorrectAnswers(triviaArticles)
+  })
+  .catch(console.log());
 })
 
-const showTriviaResults = (result) => {
+const displayTrivia = (result) => {
   let main = document.querySelector('main')
   let article = document.createElement('article');
   article.setAttribute('class', 'card');
   article.style.borderRadius = '8px';
   let h2 = document.createElement('h2');
-  h2.innerText = result.category
+  h2.textContent = result.category
   let paragraph = document.createElement('p');
-  paragraph.innerText = result.question
+  paragraph.innerHTML = result.question
   let button = document.createElement('button');
   button.innerText = 'Show Answer'
   let ansParagraph = document.createElement('p');
@@ -36,23 +38,32 @@ const showTriviaResults = (result) => {
   article.append(button)
   article.append(ansParagraph)
   main.append(article)
-  let correctAnsButtons = document.querySelectorAll('article.button')
-  for (let i = 0; i < correctAnsButtons.length; i++) {
-    const correctAnsButton = correctAnsButtons[i];
-    addEventListener('click', (event) => {
-      ansParagraph.classList.remove('hidden')
+}
+
+
+const showCorrectAnswers = (triviaArticles) => {
+  let ansParagraph = document.querySelectorAll('p.hidden')
+  let correctAnswerButtons = document.querySelectorAll('article>button')
+  for (let i = 0; i < correctAnswerButtons.length; i++) {
+    let buttonClicked = correctAnswerButtons[i];
+    buttonClicked.addEventListener('click', (e) => {
+      console.log(e)
+      ansParagraph[i].setAttribute('class', "");
     })
   }
 }
 
-const showError = (error) => {
 
-}
-
-
-const correctAnswers = (e) => {
-
-}
-const difficulty = () => {
-
+const showDifficulty = (triviaArticles) => {
+  for (let i = 0; i < triviaArticles.length; i++) {
+    let currentArticle = triviaArticles[i]
+    console.log('currentArticle: ',currentArticle)
+    if(currentArticle.difficulty === 'easy'){
+      currentArticle.setAttribute('class', "green");
+    } else if(currentArticle.difficulty === 'medium'){
+      currentArticle.setAttribute('class', "orange");
+    } else if(currentArticle.difficulty === 'hard'){
+      currentArticle.setAttribute('class', "red");
+    }
+  }
 }
